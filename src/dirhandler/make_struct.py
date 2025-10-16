7125eb8c9e3ef8127ecc8b073655cd02f53f9207
meganasampson <megan.anne.sampson@gmail.com>
2025-10-16 14:17:57 +0100

    combined replace and repeat functions in make_struct

--- src/dirhandler/make_struct.py
+++ src/dirhandler/make_struct.py
@@ -9,13 +9,16 @@ myloops = {"x":"3", "y":"2"}
 
 def replace_keys(json_struct,
                  placeholder_key):
-    for i in folder_data:
-        print(i)
+    for i in json_struct:
+        #print(i)
         res = i.translate(str.maketrans(myloops))
         print(res)
 
 def check_for_keys(json_struct,
                    placeholder_key):
+    """
+    Says if a value exists in each value in a json
+    """
     for i in json_struct:
         for key in placeholder_key.keys():
             if key in i:
@@ -25,6 +28,7 @@ def check_for_keys(json_struct,
 
 def print_multi(json_struct, 
                 placeholder_key):
+    
     for i in json_struct:
         for key in placeholder_key.keys():
             if str(key) in str(i):
@@ -34,6 +38,26 @@ def print_multi(json_struct,
             else:
                 print(f"{i} DOES NOT contain an {key}")
 
-replace_keys(folder_data, myloops)
-check_for_keys(folder_data, myloops)
-print_multi(folder_data, myloops)
+def print_numbered(json_struct,
+                   placeholder_key):
+    """
+    Loops through the json data, prints each part the specified number of times
+    from the loops dictionary.
+    """
+    
+    for i in json_struct:
+        for key in placeholder_key.keys():
+            if str(key) in str(i):
+                value = int(placeholder_key[key])
+                for k in range(value):
+                    #print(f"printing {k}")
+                    print(str(i).replace(str(key), str(k)))
+            else:
+                #print(f"{i} DOES NOT contain an {key}")
+                pass
+
+
+#replace_keys(folder_data, myloops)
+#check_for_keys(folder_data, myloops)
+#print_multi(folder_data, myloops)
+print_numbered(folder_data, myloops)
