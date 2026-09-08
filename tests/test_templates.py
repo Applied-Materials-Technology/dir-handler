f3cc413ef618bc911d016aa17ffc2518b6391f84
meganasampson <megan.anne.sampson@gmail.com>
2026-09-08 15:05:25 +0100

    remove outdated tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,22 +1,5 @@
 import pytest
 import dirhandler as dh
 
-def test_set_definition_default():
-
-    my_definitions = {"x":"3", "y":"2"}
-    default_definitions = dh.set_definitions()
-    assert my_definitions == default_definitions
-
-def test_set_definition_custom():
-
-    my_definitions = {"x":"30", "y":"20"}
-    set_definitions = dh.set_definitions(my_definitions)
-    assert my_definitions == set_definitions
-
-def test_set_single_definition():
-    
-    my_definitions = {"x":"3", "y":"2"}
-    set_definition = dh.set_definitions(my_definitions)
-    dh.set_value(set_definition, "x", "30")
-    expected_definitions = {"x":"30", "y":"2"}
-    assert set_definition == expected_definitions
+def test_named_template():
+    translations = {"testword_list": ["one", "two", "three"], "testword":"mytestword"}
